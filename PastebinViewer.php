e68d08f2944975c355090bbd351bbf85f7caba88
RaJiska <dodo.lasticot@gmail.com>
2017-09-14 14:18:04 +0200

    Enclosing BCC Frame fix

--- PastebinViewer.php
+++ PastebinViewer.php
@@ -7,13 +7,13 @@ function PastebinViewer_Process($post)
 {
 	if (!(preg_match_all('/\[pastebin\](.*?)\[\/pastebin\]/s', $post, $results)))
 		return $post;
-	foreach ($results[0] as $result)
+	foreach ($results as $key => $result)
 	{
-		preg_match('/(http|https)\:\/\/pastebin.com\/(.*?)$/s', $result, $pasteid);
+		preg_match('/(http|https)\:\/\/pastebin.com\/(.*?)$/s', $results[1][$key], $pasteid);
 		if (count($pasteid) >= 3)
 			$pasteid = $pasteid[2];
 		$replace = "<iframe src=\"https://pastebin.com/embed_iframe/$pasteid\" style=\"border:none;width:100%;height:400px\"></iframe>";
-		$post = str_replace($result, $replace, $post);
+		$post = str_replace($results[0][$key], $replace, $post);
 	}
 	return $post;
 }
